8dee5f26264e5571a9f24adf328696ca55870034
Amr Soliman <thisissoliman@protonmail.com>
2024-02-21 16:07:37 +0200

    create an endpoint for creating an issue

---
+++ app/api/issues/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+const create_issue_schema = z.object({
+  title: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  const validation = create_issue_schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
+
+  let created = await prisma.issue.create({
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(created, { status: 201 });
+}
