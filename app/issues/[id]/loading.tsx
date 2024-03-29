b41d5fa9c5527ca5530eadbdf184995d3a43d6af
Amr Soliman <thisissoliman@protonmail.com>
2024-03-29 04:38:37 +0200

    add new issue loading skeleton

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,3 +1,17 @@
+import { Box, Card, Flex } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 export default function loadingIssueDetails() {
-  return <div>loading.........</div>;
+  return (
+    <Box className="m-3 max-w-xl">
+      <Skeleton />
+      <Flex gap={"3"} my={"2"}>
+        <Skeleton width={"5rem"} />
+        <Skeleton width={"8rem"} />
+      </Flex>
+      <Card className="prose" mt={"4"}>
+        <Skeleton count={3} />
+      </Card>
+    </Box>
+  );
 }
