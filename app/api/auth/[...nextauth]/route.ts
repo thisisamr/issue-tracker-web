cbc2a14fa0455d8c3095371f021c7652b450bfda
Amr Soliman <thisissoliman@protonmail.com>
2024-04-21 01:15:34 +0200

    set up next auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import prisma from "@/prisma/client";
+
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  // Configure one or more authentication providers
+  providers: [
+    GithubProvider({
+      clientId: process.env.GITHUB_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+    // ...add more providers here
+  ],
+};
+const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
