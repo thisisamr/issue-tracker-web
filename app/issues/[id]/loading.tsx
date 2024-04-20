6bc59610c69e728d494b85703064a7c14232899c
Amr Soliman <thisissoliman@protonmail.com>
2024-04-20 22:47:45 +0200

    fix dupliate skeletons

--- app/issues/[id]/loading.tsx
+++ app/issues/[id]/loading.tsx
@@ -1,16 +1,26 @@
+"use client";
 import { Box, Card, Flex } from "@radix-ui/themes";
-import {Skeleton} from '@/app/components'
-export default function loadingIssueDetails() {
-  return (
-    <Box className="m-3 max-w-xl">
-      <Skeleton />
-      <Flex gap={"3"} my={"2"}>
-        <Skeleton width={"5rem"} />
-        <Skeleton width={"8rem"} />
-      </Flex>
-      <Card className="prose" mt={"4"}>
-        <Skeleton count={3} />
-      </Card>
-    </Box>
-  );
+import { Skeleton } from "@/app/components";
+import { usePathname } from "next/navigation";
+
+export default function LoadingIssueDetails() {
+  const p = usePathname();
+
+  console.log(!p.includes("edit"));
+  if (!p.includes("edit")) {
+    return (
+      <Box className="m-3 max-w-xl">
+        <Skeleton />
+        <Flex gap={"3"} my={"2"}>
+          <Skeleton width={"5rem"} />
+          <Skeleton width={"8rem"} />
+        </Flex>
+        <Card className="prose" mt={"4"}>
+          <Skeleton count={3} />
+        </Card>
+      </Box>
+    );
+  } else {
+    return null;
+  }
 }
--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,57 +1,58 @@
-'use client'
-import { Skeleton } from '@/app/components';
+"use client";
+import { Skeleton } from "@/app/components";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
-import { usePathname } from 'next/navigation';
+import { usePathname } from "next/navigation";
 
-
-export default function Loading() {
-    //Fix parent loading duplicate skeleton 
-const p = usePathname()
-if (p=='/issues/new'){
-    return 
-}
-    let issues = [1, 2, 3, 4, 5]
+export default function LoadingIssues() {
+  //Fix parent loading duplicate skeleton
+  const p = usePathname();
+  console.log(p);
+  if (p == "/issues") {
+    let issues = [1, 2, 3, 4, 5];
     return (
-        <div className="p-3">
-            <div className="mb-5">
-                <Button>
-                    <Link href={"/issues/new"}>New Issue</Link>
-                </Button>
-            </div>
-            <Table.Root variant="surface">
-                <Table.Header>
-                    <Table.Row>
-                        <Table.ColumnHeaderCell>Isuue</Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">
-                            Status
-                        </Table.ColumnHeaderCell>
-                        <Table.ColumnHeaderCell className="hidden md:table-cell">
-                            Created
-                        </Table.ColumnHeaderCell>
-                    </Table.Row>
-                </Table.Header>
-                <Table.Body>
-                    {issues?.map((issue) => {
-                        return (
-                            <Table.Row key={issue}>
-                                <Table.Cell>
-                                    <Skeleton />
-                                    <div className="block md:hidden">
-                                        <Skeleton />
-                                    </div>
-                                </Table.Cell>
-                                <Table.Cell className="hidden md:table-cell ">
-                                    <Skeleton />
-                                </Table.Cell>
-                                <Table.Cell className="hidden md:table-cell">
-                                    <Skeleton />
-                                </Table.Cell>
-                            </Table.Row>
-                        );
-                    })}
-                </Table.Body>
-            </Table.Root>
+      <div className="p-3">
+        <div className="mb-5">
+          <Button>
+            <Link href={"/issues/new"}>New Issue</Link>
+          </Button>
         </div>
-    )
+        <Table.Root variant="surface">
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>Isuue</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                Status
+              </Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell className="hidden md:table-cell">
+                Created
+              </Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {issues?.map((issue) => {
+              return (
+                <Table.Row key={issue}>
+                  <Table.Cell>
+                    <Skeleton />
+                    <div className="block md:hidden">
+                      <Skeleton />
+                    </div>
+                  </Table.Cell>
+                  <Table.Cell className="hidden md:table-cell ">
+                    <Skeleton />
+                  </Table.Cell>
+                  <Table.Cell className="hidden md:table-cell">
+                    <Skeleton />
+                  </Table.Cell>
+                </Table.Row>
+              );
+            })}
+          </Table.Body>
+        </Table.Root>
+      </div>
+    );
+  } else {
+    return;
+  }
 }
