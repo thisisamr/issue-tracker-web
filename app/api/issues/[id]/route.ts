b4a772f10d72c32cb0a8d4ef369a967ef75eab8b
Amr Soliman <thisissoliman@protonmail.com>
2024-04-20 14:24:43 +0200

    build a delete issue endpoint

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,21 +1,36 @@
-
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/prisma/client";
 import { create_issue_schema } from "@/app/ValidationSchemas";
-export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
-    
-    const body = await req.json();
-    const validation = create_issue_schema.safeParse(body);
-    if (!validation.success) {
-        return NextResponse.json(validation.error.errors, { status: 400 });
-    }
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const body = await req.json();
+  const validation = create_issue_schema.safeParse(body);
+  if (!validation.success) {
+    return NextResponse.json(validation.error.errors, { status: 400 });
+  }
 
-    let updated = await prisma.issue.update({where:{id:parseInt(params.id)},
-        data: {
-            title: body.title,
-            description: body.description,
-        },
+  let updated = await prisma.issue.update({
+    where: { id: parseInt(params.id) },
+    data: {
+      title: body.title,
+      description: body.description,
+    },
+  });
+
+  return NextResponse.json(updated, { status: 200 });
+}
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  try {
+    let deleted = await prisma.issue.delete({
+      where: { id: parseInt(params.id) },
     });
-
-    return NextResponse.json(updated, { status: 200 });
+    return NextResponse.json(deleted, { status: 200 });
+  } catch (error) {
+    return NextResponse.json(error, { status: 405 });
+  }
 }
