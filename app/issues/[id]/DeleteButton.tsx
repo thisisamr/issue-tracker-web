f31236637110351962cd3f9348861d82f2bd8dda
Amr Soliman <thisissoliman@protonmail.com>
2024-04-20 14:15:09 +0200

    show a confirmation dialog

--- app/issues/[id]/DeleteButton.tsx
+++ app/issues/[id]/DeleteButton.tsx
@@ -1,5 +1,32 @@
-import { Box, Button } from "@radix-ui/themes";
+"use client";
+import { AlertDialog, Box, Button, Flex } from "@radix-ui/themes";
 
 export default function DeleteButton({ id }: { id: number }) {
-  return <Button color="red">Delete Issue</Button>;
+  return (
+    <AlertDialog.Root>
+      <AlertDialog.Trigger>
+        <Button color="red">Delete Issue</Button>
+      </AlertDialog.Trigger>
+      <AlertDialog.Content>
+        <AlertDialog.Title>Revoke access</AlertDialog.Title>
+        <AlertDialog.Description size="2">
+          Are you sure you want to delete this issue? this action cannot be
+          undone.
+        </AlertDialog.Description>
+
+        <Flex gap="3" mt="4" justify="end">
+          <AlertDialog.Cancel>
+            <Button variant="soft" color="gray">
+              Cancel
+            </Button>
+          </AlertDialog.Cancel>
+          <AlertDialog.Action>
+            <Button variant="solid" color="red">
+              Delete
+            </Button>
+          </AlertDialog.Action>
+        </Flex>
+      </AlertDialog.Content>
+    </AlertDialog.Root>
+  );
 }
