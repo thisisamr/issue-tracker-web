19c147f6c868e644f90d33fd5a157635c0353e52
asolyma <asolyma@hotmail.com>
2024-06-04 13:35:51 +0300

    move auth options into seperate file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,24 +1,4 @@
 import NextAuth, { AuthOptions } from "next-auth";
-import GithubProvider from "next-auth/providers/github";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/prisma/client";
-
-const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  // Configure one or more authentication providers
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-    // GoogleProvider({
-    //   clientId: process.env.GOOGLE_CLIENT_ID!,
-    //   clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    // }),
-    // ...add more providers here
-  ],
-  session: { strategy: "jwt" },
-};
+import { authOptions } from "@/lib";
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
